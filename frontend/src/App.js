54a42589df9b35be4c0dfe9b389abcdc077f91e5
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2025-04-17 19:03:37 +0530

    feat:Added react and tailwind config

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex'>
+     <h1 className="text-5xl ">hello</h1>
+     <div className='w-full text-3xl h-full m-50 border-black-500'>
+    hii
+     </div>
+    </div>
+  );
+}
+
+export default App;
