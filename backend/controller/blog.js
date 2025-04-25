50fefd2496516a429a2bac4a4d0cdd70d74eb683
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2025-04-25 17:29:27 +0530

    feat:Added view feature and blog page

--- backend/controller/blog.js
+++ backend/controller/blog.js
@@ -6,52 +6,62 @@ import Blog from "../models/blog.js";
 
 dotenv.config();
 
-async function createBlog(req, res){
-    try{
-        const {title, category,content}=req.body;
-        const createdAt=new Date();
-        const updatedAt=new Date();
-        
+async function createBlog(req, res) {
+    try {
+        const { title, category, content } = req.body;
+        const createdAt = new Date();
+        const updatedAt = new Date();
+
         const image = req.file ? req.file.filename : null; // optional image
-        const view=0;
-        if(!title || !category ||  !content) return req.status(400).json({message:"Details are missing"});
-        const userId=req.user.userId;
-        const user=await User.findById(userId);
-        const newBlog=new Blog({title:title, category:category, content:content, image:image, author:user.name, createdAt, updatedAt, userId:userId, view:view});
-        const savedBlog=await newBlog.save();
-        return res.status(200).json({message:"Blog successfully created"});
+        const view = 0;
+        if (!title || !category || !content) return req.status(400).json({ message: "Details are missing" });
+        const userId = req.user.userId;
+        const user = await User.findById(userId);
+        const newBlog = new Blog({ title: title, category: category, content: content, image: image, author: user.name, createdAt, updatedAt, userId: userId, view: view });
+        const savedBlog = await newBlog.save();
+        return res.status(200).json({ message: "Blog successfully created" });
     }
-    catch(error){
-        return res.status(500).json({message:"Error creating Blog"});
-    }
-}
-async function getUserBlogs(req, res){
-    try{
-        const userId=req.user.userId;
-        const blogs=await Blog.find({userId:userId});
-        return res.send({blogs:blogs});
-    }
-    catch{
-        return res.status(500).json({message:"Error fetching blogs"});
+    catch (error) {
+        return res.status(500).json({ message: "Error creating Blog" });
     }
 }
-async function getAllBlogs(req, res){
-    try{
-        const blogs=Blog.find({userId:userId});
-        return res.status({blogs:blogs});
+async function getUserBlogs(req, res) {
+    try {
+        const userId = req.user.userId;
+        const blogs = await Blog.find({ userId: userId });
+        return res.send({ blogs: blogs });
     }
-    catch{
-        return res.status(500).json({message:"Error fetching blogs"});
+    catch {
+        return res.status(500).json({ message: "Error fetching blogs" });
     }
 }
-async function deleteBlog(req, res){
-    try{
-        const blogId=req.params.id;
-        const deletedUser=await Blog.findByIdAndDelete(blogId);
-        return res.status(200).json({message:"Blog deleted successfully"});
+async function getAllBlogs(req, res) {
+    try {
+        const { category, author } = req.query;
+
+        let filter = {};
+        if (category) {
+            filter.category = category;
+        }
+        if (author) {
+            filter.author = author;
+        }
+        const blogs = await Blog.find(filter);
+        console.log(blogs);
+        return res.send({ blogs: blogs });
     }
-    catch(error){
-        return res.status(500).json({message:"Error deleting blog"});
+    catch {
+        return res.status(500).json({ message: "Error fetching blogs" });
+    }
+}
+async function deleteBlog(req, res) {
+    try {
+        const blogId = req.params.id;
+        const deletedUser = await Blog.findByIdAndDelete(blogId);
+        return res.status(200).json({ message: "Blog deleted successfully" });
+    }
+    catch (error) {
+        return res.status(500).json({ message: "Error deleting blog" });
 
     }
 }
@@ -59,7 +69,7 @@ async function updateBlog(req, res) {
     try {
         console.log("hello");
 
-        const blogId = req.params.id; 
+        const blogId = req.params.id;
         const data = req.body;
 
         const updatedBlog = await Blog.findByIdAndUpdate(blogId, data, {
@@ -75,14 +85,78 @@ async function updateBlog(req, res) {
         return res.status(500).json({ error: "Something went wrong" });
     }
 }
+async function viewBlog(req, res) {
+    try {
+        const blogId = req.params.id;
+
+        const blog = await Blog.findById(blogId);
+        const previousViews = blog.view;
+        const currentViews = previousViews + 1;
+
+        blog.view = currentViews;
+        const updatedBlog = await blog.save();
+
+        return res.status(200).json({
+            message: "Blog view updated successfully",
+            updatedBlog,
+        });
+    } catch (error) {
+        console.error("Error updating blog:", error);
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+}
+async function topViewBlog(req, res) {
+    try {
+        const topBlog = await Blog.findOne().sort({ view: -1 }).limit(1);
+
+        if (!topBlog) {
+            return res.status(404).json({ message: "No blogs found" });
+        }
+
+        return res.status(200).json(topBlog);
+    } catch (error) {
+        console.error("Error finding top view blog:", error);
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+}
+async function topTenViewBlog(req, res) {
+    try {
+        const topBlogs = await Blog.find().sort({ view: -1 }).limit(10);
+
+        if (!topBlogs || topBlogs.length === 0) {
+            return res.status(404).json({ message: "No blogs found" });
+        }
+
+        return res.status(200).json(topBlogs);
+    } catch (error) {
+        console.error("Error finding top view blogs:", error);
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+}
+async function getSelectedBlog(req, res) {
+    try {
+        const blogId = req.params.id;
+        const blog = await Blog.findById(blogId);
+        res.status(200).send(blog);
+
+    } catch (error) {
+        console.error("Error details about blog:", error);
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+}
 
 
-const blogController={
-    getAllBlogs:getAllBlogs,
-    getUserBlogs:getUserBlogs,
-    createBlog:createBlog,
-    deleteBlog:deleteBlog,
-    updateBlog:updateBlog
+
+const blogController = {
+    getAllBlogs: getAllBlogs,
+    getUserBlogs: getUserBlogs,
+    createBlog: createBlog,
+    deleteBlog: deleteBlog,
+    updateBlog: updateBlog,
+    viewBlog: viewBlog,
+    topViewBlog: topViewBlog,
+    topTenViewBlog: topTenViewBlog,
+    getSelectedBlog:getSelectedBlog
 }
 
 export default blogController;
--- backend/route/blog.js
+++ backend/route/blog.js
@@ -7,8 +7,12 @@ const router=express.Router();
 
 
 router.post("/blogs", verifyToken, upload.single('image'), blogController.createBlog);
-router.get("/blogs", verifyToken, blogController.getUserBlogs);
+router.get("/blogs/user", verifyToken, blogController.getUserBlogs);
+router.get("/blogs", verifyToken, blogController.getAllBlogs);
 router.delete("/blogs/:id", verifyToken, blogController.deleteBlog);
 router.put("/blogs/:id", verifyToken, upload.single('image'), blogController.updateBlog);
-
+router.get("/views/:id", verifyToken, blogController.viewBlog);
+router.get("/topblogs", verifyToken, blogController.topViewBlog)
+router.get("/toptenblogs", verifyToken, blogController.topTenViewBlog);
+router.get("/blogs/:id", verifyToken, blogController.getSelectedBlog);
 export default router;
